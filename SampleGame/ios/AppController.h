65958a5c0d99a54770e3604aea7c1a945a2d9695
pri <pri@pri-no-MacBook-Air.local>
2012-11-01 16:09:41 +0900

    Initial Commit

---
+++ SampleGame/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  SampleGameAppController.h
+//  SampleGame
+//
+//  Created by pri on 12/11/01.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@end
+
